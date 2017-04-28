009e77a3e33422c6d36e89ef7276333d686f17fb
Daniel Penna <spardanfix@gmail.com>
2017-04-27 19:06:00 -0500

    Anadiendo modulo poll 2

--- public/themes/material/client/app/app.module.js
+++ public/themes/material/client/app/app.module.js
@@ -18,9 +18,9 @@
         'app.modules.polls.poll-answer',
         'app.modules.polls.poll-item',
         'app.modules.polls.poll-question',
-        'app.modules.polls.poll-subquestion',
-        'app.modules.polls.poll-question-type',
-        'app.modules.polls.poll-types',
+        'app.modules.polls.pollSubquestion',
+        'app.modules.polls.pollQuestionTypes',
+        'app.modules.polls.pollType',
         'app.modules.polls.poll-campaing',
         'app.health-history',
         'app.userManual',
@@ -31,7 +31,8 @@
         'mgo-angular-wizard',
         'ui.tree',
         'ngMap',
-        'textAngular'
+        'textAngular',
+        'angularMoment'
     ]);
 
 })();
