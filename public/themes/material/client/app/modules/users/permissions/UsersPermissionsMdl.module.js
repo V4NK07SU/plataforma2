ddbe3b0f3461bf917cf75aa6fd98c656fba6d26d
Pierre Silva <pierremichelsilva@gmail.com>
2017-05-12 17:31:14 -0500

    0002

--- public/themes/material/client/app/modules/users/permissions/UsersPermissionsMdl.module.js
+++ public/themes/material/client/app/modules/users/permissions/UsersPermissionsMdl.module.js
@@ -99,21 +99,3 @@
         ];
     }
 })();
-(function(){
-    'use strict';
-
-    angular
-        .module('module')
-        .controller('controller', controller)
-
-    controller.$inject = ['$location'];
-
-    function controller($location) {
-        /* jshint validthis:true */
-        var vm = this;
-
-        activate();
-
-        function activate() { }
-    }
-})();
